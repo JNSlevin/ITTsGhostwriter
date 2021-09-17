fdb471ea782a3e554debd6482c87148656c517cd
JNSlevin <84549961+JNSlevin@users.noreply.github.com>
2021-09-17 18:16:22 +0200

    Update alerts.lua

--- alerts.lua
+++ alerts.lua
@@ -8,75 +8,70 @@ local GW = {
 
 worldName = GetWorldName()
 
-function NoteAlert(_, guildId, displayName, note)
+local function NoteAlert(_, guildId, displayName, note)
     local name = GetGuildName(guildId)
     LibGuildRoster:Refresh()
-
-    -- if db.generalSettings.alerts == true then
-    chat:Print("Membernote updated for |cffffff" .. ZO_LinkHandler_CreateDisplayNameLink(displayName) .. "|r in |cffffff" .. CreateGuildLink(guildId))
-    -- end
-    if Ghostwriter.Vars.guilds[guildId].settings.autobackup == true then
-        Ghostwriter.Vars.savednotes[worldName][guildId][displayName] = note
-        LibGuildRoster:Refresh()
+    GetNotingPermission(guildId)
+    GetMailingPermission(guildId)
+    GetChatMessagePermission(guildId)
+    if Ghostwriter.Vars.guilds[guildId].settings.noteAlert == true then
+        chat:Print("Membernote updated for |cffffff" .. ZO_LinkHandler_CreateDisplayNameLink(displayName) .. "|r in |cffffff" .. CreateGuildLink(guildId))
+        -- end
+        if Ghostwriter.Vars.guilds[guildId].settings.autobackup == true then
+            Ghostwriter.Vars.savednotes[worldName][guildId][displayName] = note
+            LibGuildRoster:Refresh()
+        end
     end
 end
--- EVENT_GUILD_MEMBER_NOTE_CHANGED (number eventCode, number guildId, string displayName, string note)
 
-local function ApplicationAlert(_, guildId, numApplications)
+function LoginAlert()
+    for g = 1, GetNumGuilds() do
+        local guildId = GetGuildId(g)
+
+        ApplicationAlert(_, guildId)
+    end
+end
+function ApplicationAlert(_, guildId)
     LibGuildRoster:Refresh()
-    -- local _, level, cp, _, _, _, _, playerName, _, _, message = GetGuildFinderAccountApplicationInfo(i)
     local lvl, cp, _, _, playerName, _, _, message = GetGuildFinderGuildApplicationInfoAt(guildId, i)
     -- level,  championPoints,  Alliance alliance,  classId,  accountName,  characterName,  achievementPoints,  applicationMessage
     local name = CreateGuildLink(guildId)
+    local numApplications = GetGuildFinderNumGuildApplications(guildId)
     local nEmpty = 0
     local threshhold = Ghostwriter.Vars.guilds[guildId].settings.applicationThreshhold
     local overThreshhold = 0
     -- chat:Print("started")
-    for i = 1, numApplications do
-        local lvl, cp, _, _, playerName, _, _, message = GetGuildFinderGuildApplicationInfoAt(guildId, i)
-        chat:Print(message .. playerName)
-        if message == "" then
-            nEmpty = nEmpty + 1
-        end
-        if lvl == 50 and cp >= threshhold then
-            overThreshhold = overThreshhold + 1
-        end
-        -- return nEmpty
+    if DoesPlayerHaveGuildPermission(guildId, GUILD_PERMISSION_MANAGE_APPLICATIONS) == true then
+        for i = 1, numApplications do
+            local lvl, cp, _, _, playerName, _, _, message = GetGuildFinderGuildApplicationInfoAt(guildId, i)
 
-        -- chat:Print("first thing done")
+            if message == "" then
+                nEmpty = nEmpty + 1
+            end
+            if lvl == 50 and cp >= threshhold then
+                overThreshhold = overThreshhold + 1
+            end
+        end
+        if Ghostwriter.Vars.guilds[guildId].settings.applicationAlert == true then
+            if numApplications ~= 0 then
+                if threshhold < 1 then
+                    chat:Print("You have |cffffff" .. numApplications .. "|r open Applications (|cffffff" .. nEmpty .. "|r empty) in " .. name)
+                else
+                    chat:Print(
+                        "You have |cffffff" ..
+                            numApplications ..
+                                "|r open Applications (|cffffff" ..
+                                    nEmpty .. "|r empty and |cffffff" .. overThreshhold .. "|r over |cffffff" .. threshhold .. "|r CP) in " .. name
+                    )
+                end
+            end
+        end
     end
-    --[[  for i = 1, numApplications do
-        -- chat:Print("name = " .. playerName .. " lvl = " .. lvl .. " cp = " .. cp)
-        if lvl == 50 and cp >= threshhold then
-            overThreshhold = overThreshhold + 1
-        end
-        -- chat:Print("name = " .. playerName .. " lvl = " .. lvl .. " cp = " .. cp)
-    end ]]
-    chat:Print(
-        "You have |cffffff" ..
-            numApplications ..
-                "|r open Applications in " ..
-                    name ..
-                        ". There are currently|cffffff " .. nEmpty .. "|r empty applications and |cffffff" .. overThreshhold .. "|r over |cffffff" .. threshhold .. "|r CP!"
-    )
-end
---EVENT_GUILD_FINDER_GUILD_NEW_APPLICATIONS (number eventCode, number guildId, number numApplications)
--- SLASH_COMMANDS["/app"] = ApplicationAlert
-
-local function MailAlert()
-    LibGuildRoster:Refresh()
-    chat:Print("Mail sent")
-end
-
-local function MailAlert2()
-    LibGuildRoster:Refresh()
-    chat:Print("Mail not sent")
 end
 
 -----------
 --EVENTS
 -----------
-EVENT_MANAGER:RegisterForEvent(GW.name, EVENT_MAIL_SEND_SUCCESS, MailAlert)
-EVENT_MANAGER:RegisterForEvent(GW.name, EVENT_MAIL_SEND_FAILED, MailAlert2)
+
 EVENT_MANAGER:RegisterForEvent(GW.name, EVENT_GUILD_MEMBER_NOTE_CHANGED, NoteAlert)
 EVENT_MANAGER:RegisterForEvent(GW.name, EVENT_GUILD_FINDER_GUILD_NEW_APPLICATIONS, ApplicationAlert)
