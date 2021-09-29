7d7edcd69473a91a8461495c97ec1b81c09e3cee
JNSlevin <84549961+JNSlevin@users.noreply.github.com>
2021-09-29 19:28:44 +0200

    Update alerts.lua

--- alerts.lua
+++ alerts.lua
@@ -1,8 +1,8 @@
-local chat = LibChatMessage("Ghostwriter", "GW") -- long and short tag to identify who is printing the message
-local chat = chat:SetTagColor("04B4AE")
--- Ghostwriter = {}
+local chat = LibChatMessage("ITTsGhostwriter", "GW") -- long and short tag to identify who is printing the message
+local chat = chat:SetTagColor(GW_COLOR)
+-- ITTsGhostwriter = {}
 local GW = {
-    name = "Ghostwriter",
+    name = "ITTsGhostwriter",
     version = 0.1
 }
 
@@ -11,15 +11,19 @@ worldName = GetWorldName()
 local function NoteAlert(_, guildId, displayName, note)
     local name = GetGuildName(guildId)
     LibGuildRoster:Refresh()
-    GetNotingPermission(guildId)
-    GetMailingPermission(guildId)
-    GetChatMessagePermission(guildId)
-    if Ghostwriter.Vars.guilds[guildId].settings.noteAlert == true then
-        chat:Print("Membernote updated for |cffffff" .. ZO_LinkHandler_CreateDisplayNameLink(displayName) .. "|r in |cffffff" .. CreateGuildLink(guildId))
-        -- end
-        if Ghostwriter.Vars.guilds[guildId].settings.autobackup == true then
-            Ghostwriter.Vars.savednotes[worldName][guildId][displayName] = note
-            LibGuildRoster:Refresh()
+    GetGWNotingPermission(guildId)
+    GetGWMailingPermission(guildId)
+    GetGWChatPermission(guildId)
+    if DoesPlayerHaveGuildPermission(guildId, GUILD_PERMISSION_NOTE_EDIT) == true then
+        if ITTsGhostwriter.Vars.guilds[guildId].settings.noteAlert == true then
+            chat:Print(
+                "Membernote updated for |cffffff" .. ZO_LinkHandler_CreateDisplayNameLink(displayName) .. "|r in |cffffff" .. CreateGuildLink(guildId)
+            )
+            -- end
+            if ITTsGhostwriter.Vars.guilds[guildId].settings.autobackup == true then
+                GWData[worldName].guilds.savedNotes[guildId][displayName] = note
+                LibGuildRoster:Refresh()
+            end
         end
     end
 end
@@ -38,31 +42,33 @@ function ApplicationAlert(_, guildId)
     local name = CreateGuildLink(guildId)
     local numApplications = GetGuildFinderNumGuildApplications(guildId)
     local nEmpty = 0
-    local threshhold = Ghostwriter.Vars.guilds[guildId].settings.applicationThreshhold
-    local overThreshhold = 0
+    local threshold = ITTsGhostwriter.Vars.guilds[guildId].settings.applicationThreshold
+    local overthreshold = 0
     -- chat:Print("started")
     if DoesPlayerHaveGuildPermission(guildId, GUILD_PERMISSION_MANAGE_APPLICATIONS) == true then
-        for i = 1, numApplications do
-            local lvl, cp, _, _, playerName, _, _, message = GetGuildFinderGuildApplicationInfoAt(guildId, i)
+        if numApplications > 0 then
+            for i = 1, numApplications do
+                local lvl, cp, _, _, playerName, _, _, message = GetGuildFinderGuildApplicationInfoAt(guildId, i)
 
-            if message == "" then
-                nEmpty = nEmpty + 1
+                if message == "" then
+                    nEmpty = nEmpty + 1
+                end
+                if lvl == 50 and cp >= threshold then
+                    overthreshold = overthreshold + 1
+                end
             end
-            if lvl == 50 and cp >= threshhold then
-                overThreshhold = overThreshhold + 1
-            end
-        end
-        if Ghostwriter.Vars.guilds[guildId].settings.applicationAlert == true then
-            if numApplications ~= 0 then
-                if threshhold < 1 then
-                    chat:Print("You have |cffffff" .. numApplications .. "|r open Applications (|cffffff" .. nEmpty .. "|r empty) in " .. name)
-                else
-                    chat:Print(
-                        "You have |cffffff" ..
-                            numApplications ..
-                                "|r open Applications (|cffffff" ..
-                                    nEmpty .. "|r empty and |cffffff" .. overThreshhold .. "|r over |cffffff" .. threshhold .. "|r CP) in " .. name
-                    )
+            if ITTsGhostwriter.Vars.guilds[guildId].settings.applicationAlert == true then
+                if numApplications ~= 0 then
+                    if threshold < 1 then
+                        chat:Print("You have |cffffff" .. numApplications .. "|r open Applications (|cffffff" .. nEmpty .. "|r empty) in " .. name)
+                    else
+                        chat:Print(
+                            "You have |cffffff" ..
+                                numApplications ..
+                                    "|r open Applications (|cffffff" ..
+                                        nEmpty .. "|r empty and |cffffff" .. overthreshold .. "|r over |cffffff" .. threshold .. "|r CP) in " .. name
+                        )
+                    end
                 end
             end
         end
