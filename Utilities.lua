2d3636c4c2cad1e320f14f0dbbff0e76346d374f
JNSlevin <84549961+JNSlevin@users.noreply.github.com>
2022-04-07 01:05:50 +0200

    Update Utilities.lua

--- Utilities.lua
+++ Utilities.lua
@@ -2,7 +2,7 @@ local GW =
     ITTsGhostwriter or
     {
         name = "ITTsGhostwriter",
-        version = 1.0,
+        version = 1.3,
         variableVersion = 194
     }
 ITTsGhostwriter = GW
